94d57c308535e28d8f7d39346a34a538b4ed8925
runor25 <oruno.awhie@gmail.com>
2024-08-03 08:37:08 +0100

    css in django

--- book/views.py
+++ book/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+#from 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('CUUCUI')
+    return render(request, 'yb-base.html')
